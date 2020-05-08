078f93bab9b18e70f77fa4d8881510301c0971e7
carsonzhu <carsonzhu@tencent.com>
2020-05-08 16:25:46 +0800

    bump patch version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='certbot-dns-tencentcloud',
-    version='1.0.4',
+    version='1.0.5',
     author='Xiangyu Zhu',
     author_email='carsonzhu@tencent.com',
     description='Tencent Cloud DNS Authenticator plugin for Certbot',
