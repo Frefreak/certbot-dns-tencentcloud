fb1736494e825a66cd1338db9ccb09e148e1942f
Frefreak <frefreak.zxy@gmail.com>
2023-08-22 00:11:16 +0800

    compatibility fix thanks for osirisinferi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='certbot-dns-tencentcloud',
-    version='2.0.1',
+    version='2.0.2',
     author='Xiangyu Zhu',
     author_email='carsonzhu@tencent.com',
     description='Tencent Cloud DNS Authenticator plugin for Certbot',
